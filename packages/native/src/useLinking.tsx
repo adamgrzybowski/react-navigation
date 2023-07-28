57fe1be943adf04c0970a77fb9304cff319bbcf9
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-07-28 15:19:34 +0200

    feat: drop stale history entries after replace

--- packages/native/src/useLinking.tsx
+++ packages/native/src/useLinking.tsx
@@ -10,6 +10,7 @@ import {
 import isEqual from 'fast-deep-equal';
 import * as React from 'react';
 
+import type { HistoryRecord } from './createMemoryHistory';
 import createMemoryHistory from './createMemoryHistory';
 import ServerContext from './ServerContext';
 import type { LinkingOptions } from './types';
@@ -73,7 +74,44 @@ type Options = LinkingOptions<ParamListBase> & {
   independent?: boolean;
 };
 
-export default function useLinking(
+const findFocusedRouteKey = (state: NavigationState) => {
+  // @ts-ignore
+  return findFocusedRoute(state)?.key;
+};
+
+const getAllStateKeys = (state: NavigationState) => {
+  let current = state;
+  const keys: string[] = [];
+
+  if (current.routes) {
+    for (let route of current.routes) {
+      keys.push(route.key);
+      if (route.state) {
+        // @ts-ignore
+        keys.push(...getAllStateKeys(route.state));
+      }
+    }
+  }
+  return keys;
+};
+
+const getStaleHistoryDiff = (
+  items: HistoryRecord[],
+  newState: NavigationState
+) => {
+  const newStateKeys = getAllStateKeys(newState);
+  console.log(newStateKeys);
+  for (let i = items.length - 1; i >= 0; i--) {
+    const itemFocusedKey = findFocusedRouteKey(items[i].state);
+    console.log(itemFocusedKey);
+    if (newStateKeys.includes(itemFocusedKey)) {
+      return items.length - i - 1;
+    }
+  }
+  return -1;
+};
+
+export function useLinking(
   ref: React.RefObject<NavigationContainerRef<ParamListBase>>,
   {
     independent,
@@ -381,7 +419,18 @@ export default function useLinking(
           }
         } else {
           // If history length is unchanged, we want to replaceState
-          history.replace({ path, state });
+          // and remove any entries from history which focued route no longer exists in state
+          // That may happen if we replace a whole navigator.
+          const staleHistoryDiff = getStaleHistoryDiff(
+            history.items.slice(0, history.index + 1),
+            state
+          );
+          if (staleHistoryDiff <= 0) {
+            history.replace({ path, state });
+          } else {
+            await history.go(-staleHistoryDiff);
+            history.push({ path, state });
+          }
         }
       } else {
         // If no common navigation state was found, assume it's a replace
